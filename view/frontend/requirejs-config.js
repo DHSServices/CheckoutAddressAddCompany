9f3a4970bacc1a35ed082b331aaddf202634d57d
DHS Services <ict@dhs-services.nl>
2022-02-16 16:52:39 +0100

    update module 1.0.7

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -4,6 +4,9 @@ map: {
 
         'Magento_Checkout/template/shipping-address/address-renderer/default.html':
             'DHSServices_CheckoutAddressAddCompany/template/shipping-address/address-renderer/default.html',
+
+        'Magento_Checkout/template/shipping-information/address-renderer/default.html':
+            'My_Module/template/shipping-information/address-renderer/default.html'
     }
 }
 };
